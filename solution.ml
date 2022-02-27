b0083b0a1d164d981b6c4f78dd6d8e3894b4b198
Parker Gabel <parker.d.gabel@gmail.com>
2022-02-27 16:04:35 -0700

    Added a lot of logic

--- solution.ml
+++ solution.ml
@@ -7,17 +7,36 @@ let rec take k xs = match k with
 let rec drop k xs = match k with
 | 0 -> xs
 | k -> match xs with
-        | [] -> failwith "drop"
+        | [] -> []
         | _::ys -> drop (k - 1) ys
 
-let rest_after_whitspace i s = 
-  let start = i + 1 in 
-  String.sub s start (String.length s - start) 
+let insert i xs elem = (take i xs) @ [elem] @ (drop i xs)
 
-let f (accum : string list) (top : string) = match String.index_opt top ' ' with
-| Some x -> let top_after_whitespace = rest_after_whitspace x top in
-            List.
+let rest_after_whitspace s = match String.index_opt s ' ' with
+| Some x -> let start = x + 1 in 
+            Some (String.sub s start (String.length s - start))
+| None -> None
 
+let combine_indices xs = List.mapi (fun i elem -> (i, elem)) xs
+let findi_opt p xs =
+  let xs_with_indices = combine_indices xs in
+  match List.find_opt (fun (_, elem) -> p elem) xs_with_indices with
+  | Some x -> Some x
+  | None -> None
+
+let eq_after_whitespace e x = match rest_after_whitspace e with
+| Some y -> y = x
+| None -> false
+
+let minus_one_or_zero = function
+| k when k <= 0 -> 0
+| j -> j - 1
+
+let f (accum : string list) (top : string) = match rest_after_whitspace top with
+| Some x -> (match findi_opt (fun e -> not (eq_after_whitespace e x)) accum with
+            | Some (i, elem) -> if (String.length x) < (String.length elem) 
+                                then insert (minus_one_or_zero (i - 1)) accum top
+            | None -> accum @ [top])
 | None -> accum
 
 let the_stuff_to_do (x : string list) = 
