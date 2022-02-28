7cf3ecee673eb6f0b2feaaeda7fff71d5b04823e
Parker Gabel <parker.d.gabel@gmail.com>
2022-02-28 13:23:23 -0700

    Modules
    I moved things into extended module

--- solution.ml
+++ solution.ml
@@ -1,58 +1,70 @@
-let rec take k xs = match k with
+module String = struct
+  include String
+  let rest_after_whitspace s = 
+    let start = Int.succ @@ String.index s ' ' in
+    String.sub s start (String.length s - start)
+  
+  let compare_after_whitespace x y = 
+    let x_after_whitepace = rest_after_whitspace x
+    and y_after_whitepace = rest_after_whitspace y in
+    compare x_after_whitepace y_after_whitepace
+
+  let contains_whitespace = function
+    | "" -> false
+    | s -> String.contains s ' '
+
+  let ltaw x y = (compare_after_whitespace x y) = -1
+end
+
+module List = struct
+  include List
+
+  let findi elem l =
+    let rec findi' elem i = function
+    | [] -> failwith "findi"
+    | x::xs -> if x = elem then i else findi' elem (i + 1) xs
+    in
+    findi' elem 0 l
+
+  let rec take k xs = match k with
     | 0 -> []
     | k -> match xs with
            | [] -> failwith "take"
            | y::ys -> y :: (take (k - 1) ys)
 
-let rec drop k xs = match k with
-| 0 -> xs
-| k -> match xs with
+  let rec drop k xs = match k with
+  | 0 -> xs
+  | k -> match xs with
         | [] -> []
         | _::ys -> drop (k - 1) ys
 
-let insert i xs elem = (take i xs) @ [elem] @ (drop i xs)
+  let insert i xs elem = (take i xs) @ [elem] @ (drop i xs)
 
-let rest_after_whitspace s = 
-  let start = Int.succ @@ String.index s ' ' in
-  String.sub s start (String.length s - start)
+  let append_if_not_mem elem l = if List.mem elem l then l else l @ [elem]
+end
 
-let compare_after_whitespace x y = 
-  let x_after_whitepace = rest_after_whitspace x
-  and y_after_whitepace = rest_after_whitspace y in
-  compare x_after_whitepace y_after_whitepace
-
-let pred_or_zero = function
-| k when k <= 0 -> 0
-| j -> Int.pred j
-
-let findi elem l =
-  let rec findi' elem i = function
-  | [] -> failwith "findi"
-  | x::xs -> if x = elem then i else findi' elem (i + 1) xs
-  in
-  findi' elem 0 l
-
-let append_if_not_mem elem l = if List.mem elem l then l else l @ [elem]
+module Int = struct
+  include Int
+  let pred_or_zero = function
+    | k when k <= 0 -> 0
+    | j -> Int.pred j
+end
  
 let f accum top = 
-  match List.find_opt (fun y -> (compare_after_whitespace top y) = -1) accum with
-  | Some x -> let i = findi x accum in insert (pred_or_zero i) accum top
-  | None -> append_if_not_mem top accum
+  match List.find_opt (fun y -> String.ltaw top y) accum with
+  | Some x -> let i = List.findi x accum in List.insert (Int.pred_or_zero i) accum top
+  | None -> List.append_if_not_mem top accum
 
 let the_stuff_to_do x = 
   let stack = Stack.of_seq @@ List.to_seq x in
   Stack.fold f [] stack
-
-let contains_whitespace = function
-  | "" -> false
-  | s -> String.contains s ' '
 
   (* Note that the original function mutated an array in place 
     This function does as well *)
 let do_things_and_stuff (x : string array) =
   let res = x 
           |> Array.to_list 
-          |> List.filter contains_whitespace 
+          |> List.filter String.contains_whitespace 
           |> the_stuff_to_do 
           |> List.rev 
           |> Array.of_list in
